ec63d3a4ff917a1b108fb52385fbefe478e3f4ca
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2020-09-25 23:59:36 +0900

    Initial commit

---
+++ 42840.py
@@ -0,0 +1,24 @@
+def solution(answers):
+    a_1 = [1, 2, 3, 4, 5]
+    a_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    a_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    score = [0, 0, 0]
+
+    for i in range(len(answers)):
+        if answers[i] == a_1[i % len(a_1)]:
+            score[0] += 1
+        if answers[i] == a_2[i % len(a_2)]:
+            score[1] += 1
+        if answers[i] == a_3[i % len(a_3)]:
+            score[2] += 1
+    max_s = max(score)
+    winner = []
+    for i in range(3):
+        if max_s == score[i]:
+            winner.append(i+1)
+    return winner
+
+
+print(solution([1, 2, 3, 4, 5]))
+print(solution([1, 3, 2, 4, 2]))
---
+++ 42862.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 23:58:45 2020
+
+@author: dia_c
+"""
+
