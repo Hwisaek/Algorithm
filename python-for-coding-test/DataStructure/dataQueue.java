f8882a6f1d73dd18eac5298e166a82f2fd7a2a2d
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-02-21 04:44:18 +0900

    Create dataQueue.java

---
+++ python-for-coding-test/DataStructure/dataQueue.java
@@ -0,0 +1,28 @@
+package dataStructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class dataQueue {
+
+	public static void main(String[] args) {
+		// 큐 생성
+		Queue<Integer> q = new LinkedList();
+		
+        // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+		q.offer(5);	// 삽입(5)
+		q.offer(2); // 삽입(2)
+		q.offer(3); // 삽입(3)
+		q.offer(7); // 삽입(7)
+		q.poll();	// 삭제()
+		q.offer(1);	// 삽입(1)
+		q.offer(4);	// 삽입(4)
+		q.poll();	// 삭제()
+		
+		// 큐 구조: 4 - 1 - 7 - 3
+		while(!q.isEmpty()) {
+			System.out.println(q.poll()); // Queue.poll()은 큐의 맨 앞 자료를 반환하고 삭제함
+		}
+	}
+
+}
