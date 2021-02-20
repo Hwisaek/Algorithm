8f23fafb6afb92f8d4695ab1fa383f274a35705e
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-02-21 04:44:22 +0900

    Create dataStack.java

---
+++ python-for-coding-test/DataStructure/dataStack.java
@@ -0,0 +1,30 @@
+package dataStructure;
+
+import java.util.Stack;
+
+public class dataStack {
+
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<>(); // Stack 생성
+
+		s.push(5);	// 삽입(5)
+		s.push(2);	// 삽입(2)
+		s.push(2);	// 삽입(2)
+		s.push(7);	// 삽입(7)
+		s.pop();	// 삭제()
+		s.push(1);	// 삽입(1)
+		s.push(4);	// 삽입(4)
+		s.pop();	// 삭제()
+
+		// 결과 스택 구조 :
+		// 1
+		// 2
+		// 2
+		// 5
+		while (!s.empty()) { // Stack.empty(): 스택이 비어있는지를 반환
+			System.out.println(s.peek()); // Stack.peek(): 스택의 맨 위 값 반환
+			s.pop();
+		}
+	}
+
+}
