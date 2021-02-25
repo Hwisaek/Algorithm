aee90e0d25ee02932bd43cdc1d424888310a598f
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-02-25 15:03:14 +0900

    Create Java_5622.java

---
+++ acmicpc/Java_5622.java
@@ -0,0 +1,20 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+public class Java_5622 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] dial = { 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 };
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.next();
+		int result = 0;
+		for (int i = 0; i < input.length(); i++) {
+			result += dial[input.charAt(i) - 'A'];
+		}
+		System.out.println(result);
+	}
+
+}
