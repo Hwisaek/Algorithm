9aaf496a421ad70593178828fa79be50d30a58ca
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-03-23 18:56:21 +0900

    Update Java_11654.java

--- acmicpc/Java_11654.java
+++ acmicpc/Java_11654.java
@@ -6,17 +6,8 @@ public class Java_11654 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt();
-
-		String number = sc.next();
-
-		int sum = 0;
-		for (int i = 0; i < n; i++) {
-			sum += (int) number.charAt(i) - '0';
-		}
-
-		System.out.println(sum);
+		String str = sc.next();
+		System.out.printf("%d", (int) str.charAt(0));
 	}
 
 }
