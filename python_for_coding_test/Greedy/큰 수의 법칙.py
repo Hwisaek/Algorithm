8c89cd063331c3a9dbc8cbe7ea7edde7209cbaaa
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-10 10:58:50 +0900

    Update 큰 수의 법칙.py

--- python_for_coding_test/Greedy/큰 수의 법칙.py
+++ python_for_coding_test/Greedy/큰 수의 법칙.py
@@ -1,21 +1,22 @@
 import time
 
 
-def solution():
-    n, m, k = map(int, input().split())  # N: 배열의 크기, M: 숫자가 총 더해지는 횟수, K: 연속해서 더할 수 있는 횟수
-    nums = list(map(int, input().split()))  # 더해질 숫자
-
-    start = time.time()  # 시작시간
+def solution(n, m, k, nums):
     nums.sort()
 
     roop = nums[-1] * k + nums[-2]  # 한번의 루프로 가장 큰 값을 만들 수 있는 수
     i = m // (k + 1)  # roop가 더해질 횟수
     j = m % (k + 1)  # 가장 큰 수만 더해질 횟수
 
-    end = time.time()  # 종료시간
-    print('수행시간: ', end - start)  # 수행시간
-
     return roop * i + nums[-1] * j
 
 
-print(solution())
+n, m, k = 5, 8, 3  # N: 배열의 크기, M: 숫자가 총 더해지는 횟수, K: 연속해서 더할 수 있는 횟수
+nums = [2, 4, 5, 4, 6]  # 더해질 숫자
+
+start = time.time()  # 시작시간
+
+print(solution(n, m, k, nums))
+
+end = time.time()  # 종료시간
+print('수행시간: ', end - start)  # 수행시간
