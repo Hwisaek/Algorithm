4402d8a40ce72f45d3dcf1fca95458a9ba5fff47
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-10 19:50:56 +0900

    Create 단속카메라.py

---
+++ programmers/level3/단속카메라.py
@@ -0,0 +1,17 @@
+def solution(routes):
+    answer = 0
+    cam = -30000
+    routes.sort(key=lambda x: x[1])  # 진출지점 기준 정렬
+    chk = [False] * len(routes)  # 카메라 단속 체크
+
+    for idx, route in enumerate(routes):
+        if not chk[idx]:  # 카메라를 만나지 않았으면 실행
+            cam = route[1]  # 카메라를 해당 차량의 진출지점에 설치
+            for i, r in enumerate(routes):
+                if r[0] <= cam <= r[1]:
+                    chk[i] = True
+            answer += 1
+    return answer
+
+
+print(solution([[-20, 15], [-14, -5], [-18, -13], [-5, -3]]))
