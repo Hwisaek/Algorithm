69a857090ba9282a936c372716cf866404b692d8
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-11 12:40:15 +0900

    Update 문자열 뒤집기.py

--- python_for_coding_test/Greedy/문자열 뒤집기.py
+++ python_for_coding_test/Greedy/문자열 뒤집기.py
@@ -1,13 +1,14 @@
 def solution(s):
-    map = {"0": 0, "1": 0}  # 연속된 0과 1의 개수
-    last = -2
+    count = {"0": 0, "1": 0}  # 연속된 0과 1의 개수
+    last = -1  # 최근 숫자. 0과 1만 존재하므로 -1로 초기화
     for i in s:
-        if last != i:
-            map[i] += 1
-            last = i
-    return min(map.values())
+        if last != i:  # 연속되지 않은 값이 나오면 실행. ex) 0000'1'
+            count[i] += 1  # 해당하는 i의 값 추가
+            last = i  # 다른 값이 나왔으므로, 최근 숫자 갱신
+    return min(count.values())
 
 
-print(solution("0001100"))
-print(solution("1000110"))
-print(solution("0110110110111111110111111"))
+print(solution("0001100"))  # 1
+print(solution("1000110"))  # 2
+print(solution("0110110110111111110111111"))  # 5
+print(solution("0" * int(1e7)))  # 0
