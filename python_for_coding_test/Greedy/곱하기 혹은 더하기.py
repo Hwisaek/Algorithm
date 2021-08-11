84874f25d405403f0a3f89e6f4ecad7a87592c66
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-11 10:59:58 +0900

    Update 곱하기 혹은 더하기.py

--- python_for_coding_test/Greedy/곱하기 혹은 더하기.py
+++ python_for_coding_test/Greedy/곱하기 혹은 더하기.py
@@ -1,5 +1,6 @@
 def solution(s):
     answer = 0
+
     for i in s:
         i = int(i)
         if answer <= 1 or i <= 1:  # 0이면 곱할 수 없고, 1은 더하는게 더 좋으므로 더하기
@@ -10,8 +11,10 @@ def solution(s):
     return answer
 
 
-print(solution("02984"))
-print(solution("567"))
-print(solution("0"))
-print(solution("12"))
-print(solution("11111"))
+print(solution("02984"))  # 576
+print(solution("567"))  # 210
+print(solution("0"))  # 0
+print(solution("12"))  # 3
+print(solution("11111"))  # 5
+print(solution("103"))  # 4
+print(solution("203"))  # 6
