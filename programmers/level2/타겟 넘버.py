f6839c984d60e3001242adb697246bc4360fdaf9
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-13 14:45:14 +0900

    Create 타겟 넘버.py

---
+++ programmers/level2/타겟 넘버.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+def solution(numbers, target):
+    answer = 0
+    q = deque()
+
+    for number in numbers:
+        q
+
+    return answer
+
+
+print(solution([1, 1, 1, 1, 1]))
