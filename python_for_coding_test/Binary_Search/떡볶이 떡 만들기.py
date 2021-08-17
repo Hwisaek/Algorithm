2241c096bb9dbeefe6777c3d4bef62de9afab2b2
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-17 15:23:33 +0900

    Update 떡볶이 떡 만들기.py

--- python_for_coding_test/Binary_Search/떡볶이 떡 만들기.py
+++ python_for_coding_test/Binary_Search/떡볶이 떡 만들기.py
@@ -1,12 +1,19 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        total = sum(list(map(lambda x: x - mid if x > mid else 0, array)))
+        if total == target:
+            return mid
+        elif total > target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return None
+
+
 def solution(n, m, arr):
     arr.sort()
-    height = arr[-1] + 1
-    total = 0
-    while total < m:
-        height -= 1
-        l = list(map(lambda x: x - height if x > height else 0, arr))
-        total = sum(l)
-    return height
+    return binary_search(arr, m, 0, arr[-1] + 1)
 
 
 print(solution(4, 6, [19, 15, 10, 17]))
