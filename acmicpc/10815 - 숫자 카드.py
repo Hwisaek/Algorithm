92f3f1bef82ec9006a8ef27c8b645d29e5147362
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-17 16:01:44 +0900

    Update 10815 - 숫자 카드.py

--- acmicpc/10815 - 숫자 카드.py
+++ acmicpc/10815 - 숫자 카드.py
@@ -1,10 +1,10 @@
 n = input()
-nlst = list(map(int, input().split()))
+nlst = set(map(int, input().split()))
 m = input()
 mlst = list(map(int, input().split()))
 
 for i in mlst:
-    if nlst.count(i) > 0:
+    if i in nlst:
         print(1, end=" ")
     else:
         print(0, end=" ")
