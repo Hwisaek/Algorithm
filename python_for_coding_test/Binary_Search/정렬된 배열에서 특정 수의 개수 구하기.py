f79c57c268d31ca08dd1ef3debdb480f1e3482a6
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-18 09:35:13 +0900

    Update 정렬된 배열에서 특정 수의 개수 구하기.py

--- python_for_coding_test/Binary_Search/정렬된 배열에서 특정 수의 개수 구하기.py
+++ python_for_coding_test/Binary_Search/정렬된 배열에서 특정 수의 개수 구하기.py
@@ -1,29 +1,9 @@
+from bisect import bisect_left, bisect_right
+
 N, x = map(int, input().split())
 array = list(map(int, input().split()))
 
-a = 0
-b = 0
-
-start = 0
-end = N
-
-while start < end:
-    mid = (start + end) // 2
-    if array[mid] >= x:
-        end = mid - 1
-    else:
-        start = mid + 1
-        a = start
-
-start = 0
-end = N
-while start < end:
-    mid = (start + end) // 2
-    if array[mid] <= x:
-        start = mid + 1
-        b = start
-    else:
-        end = mid - 1
-
-answer = b - a if b - a > 0 else -1
+left = bisect_left(array, x)
+right = bisect_right(array, x)
+answer = right - left if right - left > 0 else -1
 print(answer)
