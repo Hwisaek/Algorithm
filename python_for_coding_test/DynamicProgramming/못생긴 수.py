f4fec5780cb9a865292958c7b9cbbad288daa930
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-19 14:18:24 +0900

    Create 못생긴 수.py

---
+++ python_for_coding_test/DynamicProgramming/못생긴 수.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+prime = [False] * 1001
+prime[0] = False
+prime[1] = True
+
+for i in [2, 3, 5]:
+    for j in range(i, 1001, i):
+        prime[j] = True
+
+idx = 0
+while n != 0:
+    idx += 1
+    if prime[idx]:
+        n -= 1
+
+print(idx)
