1709c0a660bb9be987ffb43549a33bf0f85c4309
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-20 15:48:52 +0900

    코딩 노트 정리

--- Note/DFS_BFS/BFS.py
+++ Note/DFS_BFS/BFS.py
@@ -1,20 +1,37 @@
-def bfs(x, y):
-    q = deque()
+from collections import deque
 
-    q.append([x, y])
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 범위 벗어나면 스킵
-            if not 0 <= nx < n or not 0 <= ny < m:
-                continue
-            # 벽이면 스킵
-            if map[nx][ny] == 0:
-                continue
-            if (nx != 0 or ny != 0) and map[nx][ny] == 1:
-                map[nx][ny] = map[x][y] + 1
-                q.append([nx, ny])
 
-    return map[n - 1][m - 1]
+# BFS 함수 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [[],
+         [2, 3, 8],
+         [1, 7],
+         [1, 4, 5],
+         [3, 5],
+         [3, 4],
+         [7],
+         [2, 6, 8],
+         [1, 7]]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 BFS 함수 호출
+bfs(graph, 1, visited)
