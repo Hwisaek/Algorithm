3bba9c47bb78931e43bb1ebf6e4b93f150e2a19c
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-24 10:22:37 +0900

    Update 16236 - 아기 상어.py

--- acmicpc/16236 - 아기 상어.py
+++ acmicpc/16236 - 아기 상어.py
@@ -1,80 +1,90 @@
-import copy
-from collections import defaultdict
-import heapq
 from collections import deque
 
-N = int(input())
+INF = 1e9  # 무한을 의미하는 값으로 10억을 설정
 
-time = 0  # 움직인 시간
+# 맵의 크기 N 입력
+n = int(input())
 
-
-# BFS 함수 정의
-def bfs(graph, start, target, size):
-    global N
-    global time, x, y
-
-    map = copy.deepcopy(graph)
-    map[start[0]][start[1]] = 0
-
-    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    queue = deque([start])
-
-    # 현재 노드를 방문 처리
-    visited = [[False] * N for _ in range(N)]
-    visited[start[0]][start[1]] = True
-
-    # 큐가 빌 때까지 반복
-    while queue:
-        # 큐에서 하나의 원소를 뽑아 출력
-        a, b = queue.popleft()
-
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-        for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if not visited[nx][ny] and graph[nx][ny] <= size:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = True
-                    map[nx][ny] = map[a][b] + 1
-                    if 0 < graph[nx][ny] < size:  # 해당 물고기를 먹을 수 있으면 True 반환
-                        time += map[nx][ny]
-                        graph[nx][ny] = 0
-                        x, y = nx, ny
-                        return True
-    return False  # 해당 물고기를 먹을 수 없으면 False
-
-
+# 전체 모든 칸에 대한 정보 입력
 array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
 
-size = 2  # 아기 상어의 크기
-x, y = -1, -1  # 아기 상어의 좌표
+# 아기 상어의 현재 크기 변수와 현재 위치 변수
+now_size = 2
+now_x, now_y = -1, -1
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-fish = []  # 물고기
-for i in range(N):
-    data = list(map(int, input().split()))
-    array.append(data)
-    for j in range(N):
-        if 0 < data[j] < 9:  # 물고기면 추가
-            fish.append((data[j], i, j))
-        if data[j] == 9:  # 아기 상어의 위치
-            x = i
-            y = j
-fish.sort(reverse=True)
-
-count = 0
-while fish:
-    if fish[-1][0] < size:  # 물고기를 잡아 먹을 수 있으면 해당 물고기 탐색
-        f = fish.pop()
-        if bfs(array, (x, y), f, size):
-            count += 1
-            if count == size:
-                size += 1
-                count = 0
-    else:  # 더이상 먹을 수 없으면 종료
+# 아기 상어의 시작 위치를 찾은 뒤에 그 위치엔 아무것도 없다고 처리
+for i in range(n):
+    for j in range(n):
+        if array[i][j] == 9:
+            now_x, now_y = i, j
+            array[now_x][now_y] = 0
+            break
+    if now_x != -1:
         break
 
-print(time)
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+# 모든 위치까지의 '최단 거리만' 계산하는 BFS 함수
+def bfs():
+    # 값이 -1이라면 도달할 수 없다는 의미 (초기화)
+    dist = [[-1] * n for _ in range(n)]
+    # 시작 위치는 도달이 가능하다고 보며 거리는 0
+    q = deque([(now_x, now_y)])
+    dist[now_x][now_y] = 0
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx and nx < n and 0 <= ny and ny < n:
+                # 자신의 크기보다 작거나 같은 경우에 지나갈 수 있음
+                if dist[nx][ny] == -1 and array[nx][ny] <= now_size:
+                    dist[nx][ny] = dist[x][y] + 1
+                    q.append((nx, ny))
+    # 모든 위치까지의 최단 거리 테이블 반환
+    return dist
+
+
+# 최단 거리 테이블이 주어졌을 때, 먹을 물고기를 찾는 함수
+def find(dist):
+    x, y = 0, 0
+    min_dist = INF
+    for i in range(n):
+        for j in range(n):
+            # 도달이 가능하면서 먹을 수 있는 물고기일 때
+            if dist[i][j] != -1 and 1 <= array[i][j] and array[i][j] < now_size:
+                # 가장 가까운 물고기 한 마리만 선택
+                if dist[i][j] < min_dist:
+                    x, y = i, j
+                    min_dist = dist[i][j]
+    if min_dist == INF:  # 먹을 수 있는 물고기가 없는 경우
+        return None
+    else:
+        return x, y, min_dist  # 먹을 물고기의 위치와 최단 거리
+
+
+result = 0  # 최종 답안
+ate = 0  # 현재 크기에서 먹은 양
+
+while True:
+    # 먹을 수 있는 물고기의 위치 찾기
+    value = find(bfs())
+    # 먹을 수 있는 물고기가 없는 경우, 현재까지 움직인 거리 출력
+    if value == None:
+        print(result)
+        break
+    else:
+        # 현재 위치 갱신 및 이동 거리 변경
+        now_x, now_y = value[0], value[1]
+        result += value[2]
+        # 먹은 위치에는 이제 아무것도 없도록 처리
+        array[now_x][now_y] = 0
+        ate += 1
+        # 자신의 현재 크기 이상으로 먹은 경우, 크기 증가
+        if ate >= now_size:
+            now_size += 1
+            ate = 0
