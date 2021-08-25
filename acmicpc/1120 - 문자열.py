6e6f517f96c985d28937837e17cc380ac418757d
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-08-25 17:03:20 +0900

    Update 1120 - 문자열.py

--- acmicpc/1120 - 문자열.py
+++ acmicpc/1120 - 문자열.py
@@ -1,4 +1,5 @@
 a, b = input().split()
+
 answer = 50
 
 for i in range(len(b) - len(a) + 1):
@@ -6,7 +7,6 @@ for i in range(len(b) - len(a) + 1):
     for j in range(len(a)):
         if b[i + j] != a[j]:
             count += 1
-    if count < answer:
-        answer = count
+    answer = min(answer, count)
 
 print(answer)
