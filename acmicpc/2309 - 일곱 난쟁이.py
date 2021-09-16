07cc685f3598ffb6f91fa252b0666f2477da135b
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-09-16 15:45:03 +0900

    Create 2309 - 일곱 난쟁이.py

---
+++ acmicpc/2309 - 일곱 난쟁이.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+array = []
+
+for _ in range(9):
+    array.append(int(input()))
+
+combs = combinations(array, 7)
+
+for comb in combs:
+    if sum(comb) == 100:
+        answer = comb
+
+for c in sorted(list(answer)):
+    print(c)
