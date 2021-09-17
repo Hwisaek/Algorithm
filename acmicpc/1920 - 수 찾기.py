eb5e4e41af17b267b58b3eaaa72d32192bb89fbf
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-09-17 12:18:39 +0900

    Update 1920 - 수 찾기.py

--- acmicpc/1920 - 수 찾기.py
+++ acmicpc/1920 - 수 찾기.py
@@ -3,26 +3,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = sorted(list(map(int, input().split())))
+a = set(map(int, input().split()))
 m = int(input())
 b = list(map(int, input().split()))
 
-
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return True
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return False
-
-
 answer = ''
 for i in b:
-    if binary_search(a, i, 0, n - 1):
+    if i in a:
         answer += '1\n'
     else:
         answer += '0\n'
