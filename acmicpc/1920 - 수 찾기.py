7b42dd921b93cfc569452127350bd18ec2094a18
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-09-17 12:24:25 +0900

    Update 1920 - 수 찾기.py

--- acmicpc/1920 - 수 찾기.py
+++ acmicpc/1920 - 수 찾기.py
@@ -2,16 +2,13 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-a = set(map(int, input().split()))
-m = int(input())
-b = list(map(int, input().split()))
+n = input()
+a = set(input().split())
+m = input()
+b = input().split()
 
 answer = ''
 for i in b:
-    if i in a:
-        answer += '1\n'
-    else:
-        answer += '0\n'
+    answer += '1\n' if i in a else '0\n'
 
 print(answer)
