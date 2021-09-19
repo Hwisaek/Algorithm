d5cfba88343f32706d43a5eabbf4200fc868ee04
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-09-20 00:46:17 +0900

    Update 2667 - 단지번호붙이기.py

--- acmicpc/2667 - 단지번호붙이기.py
+++ acmicpc/2667 - 단지번호붙이기.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n = int(input())
 
 array = [list(input()) for _ in range(n)]
@@ -6,41 +8,30 @@ visited = [[False] * n for _ in range(n)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-group = 0
 counts = []
+q = deque()
 for a in range(n):
     for b in range(n):
+        if array[a][b] == '1':
+            q.append((a, b))
+            count = 1 if array[a][b] == '1' and not visited[a][b] else 0
+            visited[a][b] = True
 
-        count = 0
+            while q:
+                x, y = q.pop()
 
+                for i in range(4):
+                    nx, ny = x + dx[i], y + dy[i]
+                    if -1 < nx < n and -1 < ny < n:
+                        if not visited[nx][ny] and array[nx][ny] == '1':
+                            q.append((nx, ny))
+                            visited[nx][ny] = True
+                            count += 1
 
-        def dfs(x, y):
-            global array
-            global count
-            global visited
+            if count != 0:
+                counts.append(count)
 
-            if visited[x][y]:
-                return
-
-            visited[x][y] = True
-            if array[x][y] == '0':
-                return
-
-            count += 1
-
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if -1 < nx < n and -1 < ny < n and not visited[nx][ny] and array[nx][ny] == '1':
-                    dfs(nx, ny)
-
-
-        dfs(a, b)
-        if count != 0:
-            group += 1
-            counts.append(count)
-
-print(group)
+print(len(counts))
 
 for count in sorted(counts):
     print(count)
