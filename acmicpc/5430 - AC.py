6f1bdfcca6f21b0eec2c9b3a443c5bcbcec81e56
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-09-27 17:11:44 +0900

    Update 5430 - AC.py

--- acmicpc/5430 - AC.py
+++ acmicpc/5430 - AC.py
@@ -1,37 +1,26 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 t = int(input().rstrip())
 
 for _ in range(t):
     p = input().rstrip().replace('RR', '')
-    n = input().rstrip()
+    start = 0
+    end = int(input().rstrip())
     array = input().rstrip()[1: -1].split(',')
-    if array[0] != '':
-        array = deque(map(int, array))
-    else:
-        array.pop()
 
-    error = False
     order = 1
     for opr in p:
         if opr == 'R':
             order *= -1
         elif opr == 'D':
-            if not array:
-                error = True
-                print('error')
-                break
-            elif order == 1:
-                array.popleft()
+            if order == 1:
+                start += 1
             else:
-                array.pop()
+                end -= 1
 
-    if not error:
-        print('[', end='')
-        answer = ''
-        for i in range(len(array))[::1 * order]:
-            answer += str(array[i]) + ','
-        print(answer[:-1], end='')
-        print(']')
+    array = array[start:end]
+    if start <= end:
+        print('[' + ','.join(array[::order]) + ']')
+    else:
+        print('error')
