3165bc567b829c7d95a05bed8c31dbe6098ffebf
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-09-28 19:34:41 +0900

    Update 약수의 개수와 덧셈.py

--- programmers/level1/약수의 개수와 덧셈.py
+++ programmers/level1/약수의 개수와 덧셈.py
@@ -1,12 +1,8 @@
 def solution(left, right):
     answer = 0
-    divisors = [1] * (right + 1)
-    for n in range(2, right + 1):
-        for i in range(n, right + 1, n):
-            divisors[i] += 1
 
     for n in range(left, right + 1):
-        if divisors[n] % 2 == 0:
+        if n ** 0.5 % 1 > 0:
             answer += n
         else:
             answer -= n
