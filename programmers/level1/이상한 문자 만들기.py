d7892940bfe04225252b9012bf6867ef09a9e5f6
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-09-28 11:20:39 +0900

    Create 이상한 문자 만들기.py

---
+++ programmers/level1/이상한 문자 만들기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    s = s.split(' ')
+    for j in range(len(s)):
+        word = list(s[j])
+        for i in range(len(word)):
+            if i % 2 != 0:
+                word[i] = word[i].lower()
+            else:
+                word[i] = word[i].upper()
+        s[j] = ''.join(word)
+    return ' '.join(s)
+
+
+print(solution("try   hello world"))
+print(solution("ab ab ab ab"))
