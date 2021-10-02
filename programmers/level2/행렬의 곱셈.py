ffe44bb7f1c9bcb2b8665cde3408680d65e24dca
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-10-02 21:32:03 +0900

    Create 행렬의 곱셈.py

---
+++ programmers/level2/행렬의 곱셈.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12949
+import numpy as np
+
+
+def solution(arr1, arr2):
+    return (np.array(arr1) @ np.array(arr2)).tolist()
+
+
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
