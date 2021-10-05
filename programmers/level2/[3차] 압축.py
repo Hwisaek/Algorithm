988e8259ec846960720940b39c9ab5151bf53006
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-10-05 12:29:34 +0900

    Create [3차] 압축.py

---
+++ programmers/level2/[3차] 압축.py
@@ -0,0 +1,28 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17684
+def solution(msg):
+    answer = []
+    d = dict()
+    for c in range(ord('A'), ord('Z') + 1):
+        d[chr(c)] = c - ord('A') + 1
+    idx = 27
+    start, end = 0, 1
+
+    while end < len(msg) + 1:
+        s = msg[start:end]
+        if s in d:
+            end += 1
+        else:
+            answer.append(d[s[:-1]])
+            d[s] = idx
+            idx += 1
+            start = end - 1
+    answer.append(d[s])
+    return answer
+
+
+# print(solution("KAKAO"))
+# print(solution("TOBEORNOTTOBEORTOBEORNOT"))
+# print(solution("ABABABABABABABAB"))
+# print(solution("A"))
+print(solution(
+    "ABCDEFSDFADSFADWQAFFDNHMUJKTSAFDSSDFJHAFSDHASFKUASHFASDFLXCVCVNZXCNMCXBSJDFLHASKFJQIOGHEWOGHTIYUETRUYROQWPEOIASFDJVILXDBPEPQWEPOIUQWRYUJRTDGTRFAFCVZVCZXCCGSDFQEQFZVCVX"))
