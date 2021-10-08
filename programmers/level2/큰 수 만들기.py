fa2e3ea5858394ea9a5729709bc01128be5a6287
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-10-08 16:29:33 +0900

    Update 큰 수 만들기.py

--- programmers/level2/큰 수 만들기.py
+++ programmers/level2/큰 수 만들기.py
@@ -1,24 +1,25 @@
-import itertools
-
-
 def solution(number, k):
-    answer = 0
+    answer = []
 
-    number = list(map(int, number))
+    for n in number:
+        answer.append(int(n))
+        while True:
+            if k > 0:
+                if len(answer) > 1:
+                    if answer[-2] < answer[-1]:
+                        answer.pop(-2)
+                        k -= 1
+                    else:
+                        break
+                else:
+                    break
+            else:
+                break
 
-    max_n = 0
-    idx = -1
-    for i in range(k):
-        if number[i] > max_n:
-            idx = i
-            max_n = number[i]
-
-    number = number[idx:]
-    k -= idx
-
-    return answer
+    return ''.join(map(str, answer))[:len(number) - k]
 
 
-print(solution("1924", 2))
-print(solution("1231234", 3))
-print(solution("4177252841", 4))
+print(solution("1924", 2) == "94")
+print(solution("1231234", 3) == "3234")
+print(solution("4177252841", 4) == "775841")
+print(solution("77777", 1), solution("77777", 1) == '7777')
