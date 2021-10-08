8d2d11e73ed7fca8bab0f7b7f7206522144f219d
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-10-08 16:42:20 +0900

    Update 큰 수 만들기.py

--- programmers/level2/큰 수 만들기.py
+++ programmers/level2/큰 수 만들기.py
@@ -2,24 +2,15 @@ def solution(number, k):
     answer = []
 
     for n in number:
-        answer.append(int(n))
-        while True:
-            if k > 0:
-                if len(answer) > 1:
-                    if answer[-2] < answer[-1]:
-                        answer.pop(-2)
-                        k -= 1
-                    else:
-                        break
-                else:
-                    break
-            else:
-                break
+        while k > 0 and len(answer) > 0 and answer[-1] < n:
+            answer.pop()
+            k -= 1
+        answer.append(n)
 
-    return ''.join(map(str, answer))[:len(number) - k]
+    return ''.join(answer[:len(number) - k])
 
 
-print(solution("1924", 2) == "94")
-print(solution("1231234", 3) == "3234")
-print(solution("4177252841", 4) == "775841")
+print(solution("1924", 2), solution("1924", 2) == "94")
+print(solution("1231234", 3), solution("1231234", 3) == "3234")
+print(solution("4177252841", 4), solution("4177252841", 4) == "775841")
 print(solution("77777", 1), solution("77777", 1) == '7777')
