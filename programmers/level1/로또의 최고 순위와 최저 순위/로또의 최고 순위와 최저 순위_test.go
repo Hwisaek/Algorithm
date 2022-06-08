7073b61a94a36123a348a80a84a35f3072e1b663
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-06-09 00:02:29 +0900

    programmers / 로또의 최고 순위와 최저 순위

--- programmers/level1/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위_test.go
+++ programmers/level1/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위_test.go
@@ -1,18 +1,47 @@
-package 로또의_최고_순위와_최저_순위
+package main
 
 import (
 	"reflect"
 	"testing"
 )
 
-func solution(lottos []int, win_nums []int) (result []int) {
-	return
+func solution(lottos []int, winNums []int) []int {
+	count := 0
+	joker := 0
+	for i := 0; i < len(lottos); i++ {
+		if lottos[i] == 0 {
+			joker++
+		} else if indexOf(winNums, lottos[i]) != -1 {
+			count++
+		}
+	}
+
+	joker += count
+
+	max := 7 - joker
+	if max > 6 {
+		max = 6
+	}
+	min := 7 - count
+	if min > 6 {
+		min = 6
+	}
+	return []int{max, min}
+}
+
+func indexOf(array []int, target int) int {
+	for i, num := range array {
+		if num == target {
+			return i
+		}
+	}
+	return -1
 }
 
 func Test_solution(t *testing.T) {
 	type args struct {
-		lottos   []int
-		win_nums []int
+		lottos  []int
+		winNums []int
 	}
 	tests := []struct {
 		name string
@@ -20,36 +49,41 @@ func Test_solution(t *testing.T) {
 		want []int
 	}{
 		{
-			name: "0",
+			name: "",
 			args: args{
-				lottos:   []int{44, 1, 0, 0, 31, 25},
-				win_nums: []int{31, 10, 45, 1, 6, 19},
+				lottos:  []int{44, 1, 0, 0, 31, 25},
+				winNums: []int{31, 10, 45, 1, 6, 19},
 			},
 			want: []int{3, 5},
 		},
 		{
-			name: "1",
+			name: "",
 			args: args{
-				lottos:   []int{0, 0, 0, 0, 0, 0},
-				win_nums: []int{38, 19, 20, 40, 15, 25},
+				lottos:  []int{0, 0, 0, 0, 0, 0},
+				winNums: []int{38, 19, 20, 40, 15, 25},
 			},
 			want: []int{1, 6},
 		},
 		{
-			name: "2",
+			name: "",
 			args: args{
-				lottos:   []int{45, 4, 35, 20, 3, 9},
-				win_nums: []int{20, 9, 3, 45, 4, 35},
+				lottos:  []int{45, 4, 35, 20, 3, 9},
+				winNums: []int{20, 9, 3, 45, 4, 35},
 			},
 			want: []int{1, 1},
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := solution(tt.args.lottos, tt.args.win_nums); !reflect.DeepEqual(got, tt.want) {
+			if got := solution(tt.args.lottos, tt.args.winNums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("solution() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkSolution(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+
 	}
 }
