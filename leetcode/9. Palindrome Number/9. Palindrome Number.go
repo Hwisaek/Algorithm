886a71ebc99b9d1cea1003aadaad421e872888db
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-09-09 01:35:59 +0900

    leetcode / 9. Palindrome Number

---
+++ leetcode/9. Palindrome Number/9. Palindrome Number.go
@@ -0,0 +1,13 @@
+package ___Palindrome_Number
+
+import "strconv"
+
+func isPalindrome(x int) (isPalindrome bool) {
+	s := strconv.Itoa(x)
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
