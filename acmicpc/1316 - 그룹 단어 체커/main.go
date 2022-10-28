ad5259ee6de4c810c3ae2a8053f13a1f4b21dda9
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-10-29 02:06:09 +0900

    acmicpc - 1316

--- acmicpc/1316 - 그룹 단어 체커/main.go
+++ acmicpc/1316 - 그룹 단어 체커/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	b "bufio"
+	. "fmt"
+	"os"
+)
 
+var (
+	sc = b.NewScanner(os.Stdin)
+)
+
+func main() {
+	sc.Scan()
+
+	count := 0
+loop:
+	for sc.Scan() {
+		word := sc.Text()
+
+		m := make(map[byte]any)
+		for i := range word {
+			c := word[i]
+			if i == 0 {
+				m[c] = nil
+				continue
+			}
+			prev := word[i-1]
+			if c != prev {
+				if _, ok := m[c]; ok {
+					continue loop
+				}
+				m[prev] = nil
+			}
+		}
+		count++
+	}
+	Println(count)
 }
