111cde73770921c8e3fc2c2c4443d6eeb14598e4
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-10-28 21:30:45 +0900

    acmicpc - 2941

--- acmicpc/2941 - 크로아티아 알파벳/main.go
+++ acmicpc/2941 - 크로아티아 알파벳/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
 
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	input, _ := r.ReadString('\n')
+
+	_, _ = fmt.Fprintln(w, len(regexp.MustCompile("c=|c-|dz=|d-|lj|nj|s=|z=|[a-z]").FindAllString(strings.TrimSpace(input), -1)))
+	_ = w.Flush()
 }
