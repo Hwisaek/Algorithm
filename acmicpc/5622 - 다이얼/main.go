fd680ec01b67a2587c9f8de3be76fe8c5e16ee13
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-10-29 23:10:00 +0900

    acmicpc - 5622

--- acmicpc/5622 - 다이얼/main.go
+++ acmicpc/5622 - 다이얼/main.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
+import . "fmt"
 
-var m = []int{3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10}
+var dial = []int{3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10}
 
 func main() {
-	var word string
-	_, _ = fmt.Scanln(&word)
+	var s string
+	_, _ = Scan(&s)
 
 	time := 0
-	for _, i2 := range word {
-		time += m[i2-'A']
+	for _, c := range s {
+		time += dial[c-'A']
 	}
-	fmt.Println(time)
+	Print(time)
 }
