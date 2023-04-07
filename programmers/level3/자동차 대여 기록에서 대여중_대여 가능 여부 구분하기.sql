c901c125cc3dea0a3f77da5d5fc92786655c7ef9
Hwisaek <dia_changmin@naver.com>
2023-04-07 13:14:45 +0900

    programmers / level 3

--- programmers/level3/자동차 대여 기록에서 대여중_대여 가능 여부 구분하기.sql
+++ programmers/level3/자동차 대여 기록에서 대여중_대여 가능 여부 구분하기.sql
@@ -1,13 +1,24 @@
 SELECT
-    CAR_ID
+    car_id
   , CASE
-        WHEN END_DATE >= '2022-10-16' THEN '대여중'
+        WHEN EXISTS (
+            SELECT
+                CAR_ID
+            FROM
+                CAR_RENTAL_COMPANY_RENTAL_HISTORY b
+            WHERE
+                START_DATE <= '2022-10-16'
+                AND END_DATE >= '2022-10-16'
+                AND a.car_id = b.car_id
+        ) THEN '대여중'
         ELSE '대여 가능'
-    END AS AVAILABILITY
+    END AS availability
 FROM
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE
-    START_DATE <= '2022-10-16'
-    AND END_DATE >= '2022-10-16'
+    (
+        SELECT DISTINCT
+            car_id
+        FROM
+            CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    ) a
 ORDER BY
-    CAR_ID DESC;
+    car_id desc;
