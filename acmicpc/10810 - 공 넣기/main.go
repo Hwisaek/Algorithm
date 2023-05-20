3c9451613b103521b69e6708c8ee0b3e43b88064
Hwisaek <dia_changmin@naver.com>
2023-05-21 01:19:25 +0900

    acmicpc / 10810 - 공 넣기

--- acmicpc/10810 - 공 넣기/main.go
+++ acmicpc/10810 - 공 넣기/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"bufio"
 	. "fmt"
+	"os"
 )
 
 func main() {
-	var N, x, v int
-	m := map[int]int{}
-	Scan(&N)
-	for i := 0; i < N; i++ {
-		Scan(&x)
-		m[x]++
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var N, M, i, j, k int
+	Fscan(r, &N, &M)
+
+	m := make([]int, N+1)
+	for x := 0; x < M; x++ {
+		Fscan(r, &i, &j, &k)
+		for y := i; y <= j; y++ {
+			m[y] = k
+		}
 	}
-	Scan(&v)
-	m[v] += 0
-	Print(m[v])
+
+	for x := 1; x <= N; x++ {
+		Fprint(w, m[x], " ")
+	}
 }
