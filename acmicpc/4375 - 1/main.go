5af196137d1102e325138e2ccb608173ce1bc72d
Hwisaek <hwisaek@gmail.com>
2024-09-29 12:16:17 +0900

    acmicpc / 4375

--- acmicpc/4375 - 1/main.go
+++ acmicpc/4375 - 1/main.go
@@ -30,43 +30,42 @@ func main() {
 		n = n[2:]                    // "0." 부분 제거
 		idx := strings.Index(n, "(") // 순환 소수 시작 인덱스 찾기
 
-		a, b := big.NewInt(0), big.NewInt(0)
-		a2, b2 := big.NewInt(1), big.NewInt(1)
+		a, b := 0, 0
+		a2, b2 := 1, 1
 
 		switch {
 		case idx < 0: // 순환 소수 없음
 			ai, _ := strconv.Atoi(n)
-			a = big.NewInt(int64(ai))
-			a2 = big.NewInt(int64(math.Pow10(len(n)))) // 소수점 이하 자리수에 해당하는 분모
+			a = ai
+			a2 = int(math.Pow10(len(n))) // 소수점 이하 자리수에 해당하는 분모
 		case idx == 0: // 순환 소수만 있음
 			bi, _ := strconv.Atoi(n[1 : len(n)-1]) // 순환 소수 값
-			b = big.NewInt(int64(bi))
-			b2 = big.NewInt(int64(math.Pow10(len(n[1 : len(n)-1])))) // 순환 소수 길이만큼의 분모
-			b2 = b2.Sub(b2, big.NewInt(1))
+			b = bi
+			b2 = int(math.Pow10(len(n[1 : len(n)-1]))) // 순환 소수 길이만큼의 분모
+			b2 = b2 - 1
 		case idx > 0: // 유한 소수 + 순환 소수
 			ai, _ := strconv.Atoi(n[:idx]) // 유한 소수 값
-			a = big.NewInt(int64(ai))
+			a = ai
 			bi, _ := strconv.Atoi(n[idx+1 : len(n)-1]) // 순환 소수 값
-			b = big.NewInt(int64(bi))
-			a2 = big.NewInt(int64(math.Pow10(len(n)))) // 전체 분모
-			b2 = big.NewInt(int64(math.Pow10(len(n[idx+1 : len(n)-1]))))
-			b2 = b2.Sub(b2, big.NewInt(1))
+			b = bi
+			a2 = int(math.Pow10(len(n[:idx]))) // 전체 분모
+			b2 = int(math.Pow10(len(n[idx+1 : len(n)-1])))
+			b2 = b2 - 1
+			b2 = b2 * int(math.Pow10(idx))
 		}
 
 		// 일반분수 + 순환소수분수
-		var x, y *big.Int
-		x = new(big.Int).Set(a)                  // a를 x로 초기화
-		x.Mul(x, b2)                             // x에 b2 곱하기
-		x.Add(x, new(big.Int).Set(b).Mul(b, a2)) // x에 b * a2 더하기
-		y = new(big.Int).Set(a2)                 // y에 a2 초기화
-		y.Mul(y, b2)                             // y에 b2 곱하기
+		bigA1 := (&big.Int{}).Mul(big.NewInt(int64(a)), big.NewInt(int64(b2)))
+		bigA2 := (&big.Int{}).Mul(big.NewInt(int64(b)), big.NewInt(int64(a2)))
+		x := bigA1.Add(bigA1, bigA2)
+		y := (&big.Int{}).Mul(big.NewInt(int64(a2)), big.NewInt(int64(b2)))
 
 		// 기약분수로 만들기
-		gcd := gcd(x, y)
-
+		gcd := gcd(new(big.Int).Set(x), new(big.Int).Set(y))
 		x.Div(x, gcd)
+
 		y.Div(y, gcd)
-		fmt.Fprintf(w, "%s/%s\n", x.Text(10), y.Text(10)) // 결과 출력
+		fmt.Fprintf(w, "%s/%s\n", x, y) // 결과 출력
 	}
 }
 
@@ -75,5 +74,5 @@ func gcd(a, b *big.Int) *big.Int {
 	if b.Cmp(big.NewInt(0)) == 0 {
 		return a
 	}
-	return gcd(b, new(big.Int).Mod(a, b))
+	return gcd(b, a.Mod(a, b))
 }
