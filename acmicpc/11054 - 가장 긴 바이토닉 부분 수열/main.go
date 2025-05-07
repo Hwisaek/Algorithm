475ea1441f3e7c5ea36f18a63858a9bdc9f825ca
Hwisaek <hwisaek@gmail.com>
2025-05-08 00:58:33 +0900

    acmicpc / 11054 - 가장 긴 바이토닉 부분 수열 구현

--- acmicpc/11054 - 가장 긴 바이토닉 부분 수열/main.go
+++ acmicpc/11054 - 가장 긴 바이토닉 부분 수열/main.go
@@ -1,1 +1,34 @@
-package _1054___가장_긴_바이토닉_부분_수열
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	N := 0
+	fmt.Fscanln(reader, &N)
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(reader, &A[i])
+	}
+
+	d := map[int]int{}
+	for _, item := range A {
+		length := 0
+		for j := 1; j <= len(d); j++ {
+			if d[j] < item {
+				length = j
+			}
+		}
+
+		d[length+1] = item
+	}
+
+	fmt.Fprintln(writer, len(d))
+}
