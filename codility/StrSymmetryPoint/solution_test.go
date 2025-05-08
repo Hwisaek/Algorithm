3a97a16f8c7e1ad9e7e7c412ad4560813446f80f
Hwisaek <hwisaek@gmail.com>
2025-05-08 15:56:26 +0900

    codility / StrSymmetryPoint - 문자열의 대칭점을 찾는 로직 수정 및 테스트 케이스 개선

--- codility/StrSymmetryPoint/solution_test.go
+++ codility/StrSymmetryPoint/solution_test.go
@@ -1,18 +1,19 @@
 package solution
 
 func Solution(S string) (answer int) {
-	if len(S) == 0 {
-		return -1
-	}
 	if len(S) == 1 {
 		return 0
 	}
+
+	if len(S)%2 == 0 {
+		return -1
+	}
+
 	for i := 0; i < len(S)/2; i++ {
-		if S[i] == S[len(S)-1-i] {
-			answer++
-		} else {
-			break
+		if S[i] != S[len(S)-1-i] {
+			return -1
 		}
 	}
-	return
+
+	return len(S) / 2
 }
