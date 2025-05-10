58bc5b49852d19dfdc79e94e3c7ba898facab6ce
Hwisaek <hwisaek@gmail.com>
2025-05-10 17:24:06 +0900

    codility / TapeEquilibrium - 주어진 배열의 최소 차이를 계산하는 함수 및 테스트 케이스 추가

---
+++ codility/TapeEquilibrium/solution_test.go
@@ -0,0 +1,24 @@
+package solution
+
+func Solution(A []int) (answer int) {
+	lSum, rSum := A[0], 0
+	for i := 1; i < len(A); i++ {
+		rSum += A[i]
+	}
+
+	diff := rSum - lSum
+	for i := 1; i < len(A)-1; i++ {
+		lSum += A[i]
+		rSum -= A[i]
+		diff = min(diff, abs(rSum-lSum))
+	}
+
+	return diff
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
