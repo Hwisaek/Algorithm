5d560ff8bfcf1e62f2bd1dd3b36b4c5df540a35b
Hwisaek <hwisaek@gmail.com>
2025-05-13 00:46:31 +0900

    codility / PermCheck - 주어진 배열이 1부터 N까지의 모든 정수를 포함하는지 확인하는 함수 및 테스트 케이스 추가

---
+++ codility/PermCheck/solution_test.go
@@ -0,0 +1,62 @@
+package solution
+
+import "testing"
+
+func Solution(A []int) int {
+	MAX := 0
+
+	m := make(map[int]struct{})
+	for _, e := range A {
+		MAX = max(MAX, e)
+		m[e] = struct{}{}
+	}
+
+	if MAX == len(m) {
+		return 1
+	}
+
+	return 0
+}
+
+func TestSolution(t *testing.T) {
+	type arg struct {
+		A []int
+	}
+	type expect struct {
+		answer int
+	}
+
+	tests := []struct {
+		name   string
+		arg    arg
+		expect expect
+	}{
+		{
+			name: "",
+			arg: arg{
+				[]int{4, 1, 3, 2},
+			},
+			expect: expect{
+				answer: 1,
+			},
+		},
+		{
+			name: "",
+			arg: arg{
+				[]int{4, 1, 3},
+			},
+			expect: expect{
+				answer: 0,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Solution(tt.arg.A)
+			if got != tt.expect.answer {
+				t.Errorf("%s: Solution() = %v, want %v", tt.name, got, tt.expect.answer)
+			}
+		})
+	}
+}
